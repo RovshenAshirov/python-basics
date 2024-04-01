6961d2f53e060bd4c3ade2c498fb698e1df07a72
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-01 14:00:49 +0500

    round function

---
+++ 02-numbers/07-round-function.py
@@ -0,0 +1,14 @@
+# round function
+# round funksiyasi
+
+import math
+
+# round - tugallamoq
+print(5 / 2)  # 2.5
+print(round(5 / 2))  # 2
+print(round(10 / 3, 2))  # 3.33
+
+# ceil - shift
+print(math.ceil(10 / 3))  # 4
+# floor - pol
+print(math.floor(10 / 3))  # 3
