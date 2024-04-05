5c25ff08b0bae7c0900ff63faf03580e2e57e89d
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-05 11:41:39 +0500

    increment and decrement operators

---
+++ 02-numbers/09-increment-and-decrement-operators.py
@@ -0,0 +1,9 @@
+# Increment and decrement operators
+# O'stirish va kamaytirish operatorlari
+
+x = 10
+
+x += 3  # x = x + 3 -> increment
+x -= 3  # x = x - 3 -> decrement
+x *= 3  # x = x * 3
+x /= 3  # x = x / 3
