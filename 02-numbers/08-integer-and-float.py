b01fad09552e2b8eaddc98a59bd4b998c7b33041
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-05 11:33:59 +0500

    integer and float

---
+++ 02-numbers/08-integer-and-float.py
@@ -0,0 +1,15 @@
+# Integer and float
+# Butun va haqiqiy sonlar
+
+a = 5  # integer - butun son
+b = 4.2  # float - haqiqiy son
+
+print(a + b)  # float
+
+population = 7_594_376_567
+
+print(population)
+
+PI = 3.14  # constants - katta harflar bilan yozish konstanta ekanligini bildiradi
+
+print(PI)
