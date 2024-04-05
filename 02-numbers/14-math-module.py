0f49a0e94ccc1ff4bc60d1c43d52e89dd17fda35
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-05 12:40:02 +0500

    math module

---
+++ 02-numbers/14-math-module.py
@@ -0,0 +1,23 @@
+# math module
+# math moduli
+
+# https://docs.python.org/3/library/math.html
+import math
+
+# constants
+print(math.pi)
+print(math.e)
+
+# methods
+print(math.exp(5))  # e^5
+print(math.factorial(6))  # 6!
+print(math.sqrt(16))  # v(16)
+
+print(math.log(16, 2))  # log2(16)
+print(math.log2(16))
+print(math.log10(1000))
+
+print(math.sin(math.pi / 2))
+print(math.cos(math.pi))
+print(math.tan(0))
+
