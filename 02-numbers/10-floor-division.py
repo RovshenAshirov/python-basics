721d65b537b6799227f6c79c7ee0ab28a2fb4997
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-04-05 11:45:38 +0500

    floor division

---
+++ 02-numbers/10-floor-division.py
@@ -0,0 +1,9 @@
+# Floor division
+# Butun bo'lish
+
+import math
+
+print(10 / 3)  # float division - kasrli bo'lish
+print(10 // 3)  # integer division - butun bo'lish
+
+print(math.floor(10 / 3))  # integer division
