6c1c66f7d1d0122b1b74e735438f1916652f44cf
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-18 21:22:09 +0500

    convert integer to string

---
+++ 03-strings/15-convert-integer-to-string.py
@@ -0,0 +1,7 @@
+# Convert Integer to String
+# Satrni songa aylantirish
+
+msg = "please subscribe"
+
+print("You're message was " + str(len(msg)) + " characters long")
+print("You're message was", len(msg), "characters long")
