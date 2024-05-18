ccc3d4c8cf2b7f0d47ab1e7780ba1aaf648d5127
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-18 21:34:18 +0500

    creating a list and indexing

---
+++ 04-lists/01-creating-a-list.py
@@ -0,0 +1,14 @@
+# Creating a List and Indexing
+# Ro'yxat yaratish va uning indeksi
+
+ages = [12, 18, 28]
+people = ["John", "Sabrina", "Emily"]
+my_favorite_things = ["Working out", 7, ["amazon prime", "netflix"]]
+
+print(ages)
+print(people)
+print(my_favorite_things)
+
+print(ages[0])
+print(people[1])
+print(my_favorite_things[2])
