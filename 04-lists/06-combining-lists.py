e125a5a67f2696b4d542f0a299e23fb829d3a340
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-18 22:04:19 +0500

    combining lists

---
+++ 04-lists/06-combining-lists.py
@@ -0,0 +1,12 @@
+# Combining Lists
+# Ro'yxatlarni birlashtirish
+
+my_favorite_things = ["Working out", 7, ["amazon prime", "netflix"]]
+least_favorite_things = ["onions", "JavaScript"]
+print(my_favorite_things + least_favorite_things)
+
+least_favorite_things = least_favorite_things + ["editing"]
+print(least_favorite_things)
+
+least_favorite_things.append("editing")
+print(least_favorite_things)
