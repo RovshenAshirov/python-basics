7da9838eeeeef65db5a42d89832429f5a112f1c9
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-18 21:26:50 +0500

    formatted and unformatted string

---
+++ 03-strings/16-formatted-string.py
@@ -0,0 +1,9 @@
+# Formatted String
+# Formatlangan satr
+
+language = "Python"
+message = f"I'm a {language} programmer"
+message2 = r"C:\temp\new\n"  # unformatted string
+
+print(message)
+print(message2)
