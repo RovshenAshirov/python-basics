e3b56c8998becd08ae30a1eb47c79c60f677dafe
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-18 21:37:29 +0500

    changing and slicing a list

---
+++ 04-lists/02-changing-and-slicing.py
@@ -0,0 +1,11 @@
+# Changing and Slicing a List
+# Ro'yxatni bo'lish va o'zgartirish
+
+my_favorite_things = ["Working out", 7, ["amazon prime", "netflix"]]
+print(id(my_favorite_things))
+
+my_favorite_things[0] = "Walking"
+print(id(my_favorite_things))
+
+print(my_favorite_things)
+print(len(my_favorite_things))
